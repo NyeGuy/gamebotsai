0080c534e34e3b627b51b5339cd9f9c780b4ff98
Nye Warburton <nyewarburton@gmail.com>
2016-10-06 14:02:43 -0700

    refactoring code to get basic structure to work

--- server.js
+++ server.js
@@ -1,31 +1,20 @@
-'use strict'
-
-const express = require('express')
 const Slapp = require('slapp')
-const BeepBoopConvoStore = ('slapp-convo-beepboop')
 const BeepBoopContext = require('slapp-context-beepboop')
 if (!process.env.PORT) throw Error('PORT missing but required')
 
-var slapp = Slapp({ 
-    convo_store: BeepBoopConvoStore(),
-    context: BeepBoopContext()
-})
+var slapp = Slapp({ context: BeepBoopContext() })
 
-var app = slapp.attachToExpress(express())
-
-slapp.message('^(hi|hello|hey).*', ['direct_mention', 'direct_message'], (msg, text, match1) => {
-  msg.say(`${greeting}, how are you?`).route('handleHowAreYou', { what: match1 })  // where to route the next msg in the conversation
+slapp.message('^(hi|hello|hey).*', ['direct_mention', 'direct_message'], (msg, text, greeting) => {
+  msg
+    .say(`${greeting}, how are you?`)
+    .route('handleHowAreYou')  // where to route the next msg in the conversation
 })
 
 // register a route handler
-slapp.route('handleHowAreYou', (msg, state) => {
+slapp.route('handleHowAreYou', (msg) => {
   // respond with a random entry from array
-  msg.say([':smile:, Me too', 'Noted', 'That is interesting'])
+  msg.say(['Me too', 'Noted', 'That is interesting'])
 })
 
-app.get('/', function (req,res){
-    res.send('Hello')
-})
-
-console.log('Listening on:' + process.env.PORT)
-app.listen(process.env.PORT)
+// attach handlers to an Express app
+slapp.attachToExpress(require('express')()).listen(process.env.PORT)
