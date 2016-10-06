785a22898379aeee9a732537b5bdea39da125c34
Nye Warburton <nyewarburton@gmail.com>
2016-10-06 14:23:19 -0700

    simplified code to get basic bot to work

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
+'use strict'
+const express = require('express')
 const Slapp = require('slapp')
+const BeepBoopConvoStore = require('slapp-convo-beepboop')
 const BeepBoopContext = require('slapp-context-beepboop')
 if (!process.env.PORT) throw Error('PORT missing but required')
 
-var slapp = Slapp({ context: BeepBoopContext() })
-
-slapp.message('^(hi|hello|hey).*', ['direct_mention', 'direct_message'], (msg, text, greeting) => {
-  msg
-    .say(`${greeting}, how are you?`)
-    .route('handleHowAreYou')  // where to route the next msg in the conversation
+var slapp = Slapp({
+  convo_store: BeepBoopConvoStore(),
+  context: BeepBoopContext()
 })
 
-// register a route handler
-slapp.route('handleHowAreYou', (msg) => {
-  // respond with a random entry from array
-  msg.say(['Me too', 'Noted', 'That is interesting'])
+var app = slapp.attachToExpress(express())
+
+slapp.message('hi (.*)', ['direct_message'], (msg, text, match1) => {
+  msg.say('How are you?').route('handleHi', { what: match1 })
 })
 
-// attach handlers to an Express app
-slapp.attachToExpress(require('express')()).listen(process.env.PORT)
+slapp.route('handleHi', (msg, state) => {
+  msg.say(':smile: ' + state.what)
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello')
+})
+
+console.log('Listening on :' + process.env.PORT)
+app.listen(process.env.PORT)
